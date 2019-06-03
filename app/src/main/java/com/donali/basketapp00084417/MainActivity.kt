db605eb5d4debc799d489afa6f539b4fbf003618
Donali <00084417@uca.edu.sv>
2019-06-02 23:22:16 -0600

    UI Finished

--- app/src/main/java/com/donali/basketapp00084417/MainActivity.kt
+++ app/src/main/java/com/donali/basketapp00084417/MainActivity.kt
@@ -6,23 +6,21 @@ import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.donali.basketapp00084417.database.viewmodels.BasketViewModel
+import com.donali.basketapp00084417.fragments.TeamFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val viewModel = ViewModelProviders.of(this).get(BasketViewModel::class.java)
-
-
-        viewModel.getAllMatches().observe(this, Observer {
-            it.forEach{match->
-                Log.d("CUSTOM","start date: ${match.start_date}")
-                Log.d("CUSTOM","id: ${match.id}")
-                Log.d("CUSTOM","team id 1 ${match.id_team_1}")
-                Log.d("CUSTOM","team id 2 ${match.id_team_2}")
-            }
-        })
+        initFragments()
     }
+
+    fun initFragments(){
+        val team1 = TeamFragment()
+        val team2 = TeamFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fl_team_1,team1).commit()
+        supportFragmentManager.beginTransaction().add(R.id.fl_team_2,team2).commit()
+    }
+
 }
